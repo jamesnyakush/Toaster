342a08ef8b7ec5b431b17c9160fd9ed274b713a7
jamesnyakush <emvesttech@gmail.com>
2019-07-09 17:52:24 -0700

    Update ToasterMessage.java

--- toastylibrary/src/main/java/com/example/toastylibrary/ToasterMessage.java
+++ toastylibrary/src/main/java/com/example/toastylibrary/ToasterMessage.java
@@ -5,15 +5,17 @@ import android.support.annotation.StringRes;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ToasterMessage {
     public static final int LENGTH_SHORT = Toast.LENGTH_SHORT;
     public static final int LENGTH_LONG = Toast.LENGTH_LONG;
 
-    public static void s(Context c, String message,int duration){
+    public static void s(Context context, String message,int duration){
 
-        LayoutInflater inflater = (LayoutInflater) c.getApplicationContext()
+
+        LayoutInflater inflater = (LayoutInflater) context.getApplicationContext()
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
 
@@ -21,11 +23,15 @@ public class ToasterMessage {
 
         View toastView = inflater.inflate(R.layout.succes, null);
 
+        TextView text = (TextView) toastView.findViewById(R.id.customToastText);
+
         // Initiate the Toast instance.
-        Toast toast = new Toast(c.getApplicationContext());
+        Toast toast = new Toast(context.getApplicationContext());
 
         toast.setView(toastView);
-//        toast.setText(message);
+
+        text.setText(message);
+        
         toast.setDuration(duration);
         toast.setGravity(Gravity.BOTTOM, 0,0);
         toast.show();
